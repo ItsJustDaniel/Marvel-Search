624a5ce07dfee849de96f7f84dd11da250d68c0a
Daniel Li <danielli12750@gmail.com>
2020-12-01 19:54:30 +1100

    using https for api url

--- src/api/marvel.js
+++ src/api/marvel.js
@@ -8,7 +8,7 @@ function getMarvelCharacters(options) {
   console.log(PUBLIC_KEY);
   const ts = new Date().getTime();
   const hash = CryptoJs.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
-  const MarvelUrl = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&offset=${offset}${name}`;
+  const MarvelUrl = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&offset=${offset}${name}`;
 
   console.log(MarvelUrl);
   return fetch(MarvelUrl)
